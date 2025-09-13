0336cea7c758cf3e2c9572b76689aaaed15f29ce
Mach Hao Tuan <117436192+Huchuynh@users.noreply.github.com>
2025-09-13 20:32:57 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 set -e
 
-# Chờ Postgres sẵn sàng
-until nc -z -v -w30 $DB_HOST $DB_PORT
-do
-  echo "⏳ Waiting for Postgres at $DB_HOST:$DB_PORT..."
-  sleep 5
-done
-
-echo "✅ Database is up!"
-
 # Chạy migrate + seed StudentsSeeder
 php artisan migrate --force
 php artisan db:seed --class=StudentsSeeder --force
